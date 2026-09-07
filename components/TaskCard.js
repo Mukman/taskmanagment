33e8f17b5bac0d4a72f2f439ab17591afc406be2
Muktar Endris Abdu <muktar.endris@hijrabank.local>
2026-09-07 14:34:02 +0300

    Initial commit

--- components/TaskCard.js
+++ components/TaskCard.js
@@ -30,7 +30,12 @@ export default function TaskCard({ task, onAdvance, onDelete, onEdit, assigneeNa
               </button>
             )}
             {onDelete && (
-              <button onClick={() => onDelete(task.id)} style={{ background: "none", border: "none", cursor: "pointer", color: T.inkMuted, padding: 1, display: "flex" }}>
+              <button
+                onClick={() => {
+                  if (confirm(`Delete "${task.title}"? This can't be undone.`)) onDelete(task.id);
+                }}
+                style={{ background: "none", border: "none", cursor: "pointer", color: T.inkMuted, padding: 1, display: "flex" }}
+              >
                 <X size={13} />
               </button>
             )}
